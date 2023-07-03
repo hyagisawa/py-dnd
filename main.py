3863c86adc62c7c37bf94233175e111880a750b0
hyagisawa <h-yagisawa@premeitec.co.jp>
2023-07-03 23:38:30 +0900

    画像を追加

--- main.py
+++ main.py
@@ -8,58 +8,47 @@ class MyApp(TkinterDnD.Tk):
 
         ## ウィンドウサイズ
         width = 300
-        height = 260
+        height = 300
         self.geometry(f'{width}x{height}')
         self.minsize(width, height)
         self.maxsize(width, height)
         self.title(f'DnD')
-
-        # canvas
-        self.canvas = tk.Canvas(self)
-        
-        # canvas の配置
-        self.canvas.pack(expand=True,fill=tk.BOTH)
+       
         # ## フレーム
-        # self.frame_drag_drop = frameDragAndDrop(self)
+        self.frame_drag_drop = frameDragAndDrop(self)
 
         # ## 配置
-        # self.frame_drag_drop.grid(column=0, row=0, padx=5, pady=5, sticky=(tk.E, tk.W, tk.S, tk.N))
-        # self.columnconfigure(0, weight=1)
-        # self.rowconfigure(0, weight=1)
+        self.frame_drag_drop.grid(column=0, row=0, padx=0, pady=0, sticky=(tk.E, tk.W, tk.S, tk.N))
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
 
 
 class frameDragAndDrop(tk.LabelFrame):
     def __init__(self, parent):
         super().__init__(parent)
         
-    
-        self.textbox = tk.Text(self)
-        self.textbox.insert(0.0, "Drag and Drop")
-        self.textbox.configure(state='disabled')
-
+        # canvas
+        self.canvas = tk.Canvas(self)
+        
+        # canvas の配置
+        self.canvas.pack(expand=True,fill=tk.BOTH)
+        
+        # 画像パス
+        self.photo_image = ImageTk.PhotoImage(file = "./resource/dnd.png")
+      
+       
+        # 画像の描画
+        self.canvas.create_image(145, 145, image=self.photo_image)
+        
         ## ドラッグアンドドロップ
-        self.textbox.drop_target_register(DND_FILES)
-        self.textbox.dnd_bind('<<Drop>>', self.funcDragAndDrop)
+        self.canvas.drop_target_register(DND_FILES)
+        self.canvas.dnd_bind('<<Drop>>', self.funcDragAndDrop)
 
-        ## スクロールバー設定
-        self.scrollbar = tk.Scrollbar(self, orient=tk.VERTICAL, command=self.textbox.yview)
-        self.textbox['yscrollcommand'] = self.scrollbar.set
-
-        ## 配置
-        self.textbox.grid(column=0, row=0, sticky=(tk.E, tk.W, tk.S, tk.N))
-        self.scrollbar.grid(row=0, column=1, sticky=(tk.N, tk.S))
-        self.columnconfigure(0, weight=1)
-        self.rowconfigure(0, weight=1)
 
     def funcDragAndDrop(self, e):
         ## ここを編集してください
         message = '\n' + e.data
-
-        self.textbox.configure(state='normal')
-        self.textbox.insert(tk.END, message)
-        self.textbox.configure(state='disabled')
-
-        self.textbox.see(tk.END)
+        print(message)
 
 
 if __name__ == "__main__":
